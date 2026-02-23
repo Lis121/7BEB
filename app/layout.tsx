ae3b46670026430ad8e86407e5d08ba4b0edd4a7
Lis121 <leo00lis@gmail.com>
2026-02-23 10:52:53 +0100

    Add Microsoft Clarity tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,15 @@ export default function RootLayout({
             gtag('config', 'G-GZ1MYL8QC9');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vltbu83mqn");
+          `}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
